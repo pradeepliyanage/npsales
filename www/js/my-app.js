c1a05b356af02c53c72f907c10f31e65c96ff870
pradeepliyanage <80473515+pradeepliyanage@users.noreply.github.com>
2022-11-08 00:38:11 +0530

    np16

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -92,7 +92,7 @@ $$(document).on('pageInit', function (e) {
              var display = result;
 				// And insert generated list to page content
         $$(page.container).find('.page-content').append(display);
-		
+		$("#backbutton").css("display", "block");
 		});
        
         getLocationUpdate();//only when geo location info is required on this page
@@ -154,7 +154,7 @@ $$(document).on('pageInit', function (e) {
              var display = result;
 				// And insert generated list to page content
         $$(page.container).find('.page-content').append(display);
-				
+		$("#backbutton").css("display", "block");		
 				 });
 				 
 		
@@ -167,6 +167,8 @@ $$(document).on('pageInit', function (e) {
         
 	if(uuidglobe!='')
 	getempname(uuidglobe);
+
+	$("#backbutton").css("display", "none");
     }
 	
 	
