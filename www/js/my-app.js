5ddcf1202a17514fe59c8b48cb83e384bec5ec5f
pradeepliyanage <80473515+pradeepliyanage@users.noreply.github.com>
2022-11-08 16:15:34 +0530

    np22

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -190,7 +190,10 @@ $$(document).on('pageInit', '.page[data-page="home"]', function (e) {
     // Following code will be executed for page with data-page attribute equal to "about"
     myApp.alert('Here comes About page');
 })
-
+$$(document).on('pageInit', '.page[data-page="index"]', function (e) {
+    // Following code will be executed for page with data-page attribute equal to "about"
+    $("#gradientnavbar").css("height", "25%");
+})
 
 
 // swipe back event
