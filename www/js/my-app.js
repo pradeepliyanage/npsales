7045e083aadb865df073dd83347d71835c50eed5
pradeepliyanage <80473515+pradeepliyanage@users.noreply.github.com>
2023-02-05 13:23:37 +0530

    service 5

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -382,7 +382,7 @@ function openurl2gps(page,latlong){
 		   }); */
 		
 		// Add beforeload event handler which is called before each new URL is loaded into the InAppBrowser Webview
-		ref.addEventListener('beforeload', function(params, callback){
+		/* ref.addEventListener('beforeload', function(params, callback){
 			// If the URL being loaded is a PDF
 			alert(params.url);
 			if(params.url.match(".pdf")){
@@ -393,9 +393,53 @@ function openurl2gps(page,latlong){
 				// Invoke callback to load this URL in InAppBrowser
 				callback(params.url);
 			}
-		});  
+		});  */
+
+		
+		ref.addEventListener('loadstart', function(e) {
+			var url = e.url;
+			var extension = url.substr(url.length - 4);
+			alert(url);
+			if (extension == '.pdf') {
+			  var targetPath = cordova.file.documentsDirectory + "receipt.pdf";
+			  var options = {};
+			  var args = {
+				url: url,
+				targetPath: targetPath,
+				options: options
+			  };
+			  ref.close(); // close window or you get exception
+			  document.addEventListener('deviceready', function () {
+				setTimeout(function() {
+				  downloadReceipt(args); // call the function which will download the file 1s after the window is closed, just in case..
+				}, 1000);
+			  });
+			}
+		  });
 		
 }
+
+
+function downloadReceipt(args) {
+	var fileTransfer = new FileTransfer();
+	var uri = encodeURI(args.url);
+  
+	fileTransfer.download(
+	  uri, // file's uri
+	  args.targetPath, // where will be saved
+	  function(entry) {
+		alert("download complete: " + entry.toURL());
+		window.open(entry.toURL(), '_blank', 'location=no,closebuttoncaption=Cerrar,toolbar=yes,enableViewportScale=yes');
+	  },
+	  function(error) {
+		alert("download error source " + error.source);
+		alert("download error target " + error.target);
+		alert("upload error code" + error.code);
+	  },
+	  true,
+	  args.options
+	);
+  }
 
 function openurlgooglemap(page){
 
