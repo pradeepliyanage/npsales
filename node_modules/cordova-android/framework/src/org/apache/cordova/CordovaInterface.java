672cfaccc4407d48fa89564e0bde56a151d0776f
Pasindu Charith <hgpcsagara@gmail.com>
2026-08-03 11:54:55 +0530

    chore(config): update plugin versions to match installed dependencies
    - cordova-plugin-camera: 6.0.0 → 8.0.0
    - cordova-plugin-media-capture: 4.0.0 → 6.0.0
    - cordova-plugin-contacts: 2.0.1 → 3.0.1
    - cordova-plugin-device: 2.0.3 → 3.0.0
    - cordova-plugin-device-motion: 1.2.0 → 2.0.1
    - cordova-plugin-device-orientation: 1.0.2 → 3.0.0
    - cordova-plugin-location-provider: 1.5.4 → 1.5.7
    - cordova-plugin-inappbrowser: 5.0.0 → 7.0.0
    - cordova-plugin-media: 6.1.0 → 7.0.0
    - cordova-plugin-network-information: 3.0.0 → 3.1.0
    - cordova-plugin-statusbar: 2.4.3 → 4.0.0
    - cordova-plugin-vibration: 2.1.0 → 3.1.1
    - cordova-plugin-ionic-webview: 5.0.0 → 5.0.1
    - cordova-plugin-sms: added version spec ~1.0.5
    - cordova-sms-plugin: added version spec ~1.0.5

--- node_modules/cordova-android/framework/src/org/apache/cordova/CordovaInterface.java
+++ node_modules/cordova-android/framework/src/org/apache/cordova/CordovaInterface.java
@@ -1,20 +1,20 @@
 /*
-       Licensed to the Apache Software Foundation (ASF) under one
-       or more contributor license agreements.  See the NOTICE file
-       distributed with this work for additional information
-       regarding copyright ownership.  The ASF licenses this file
-       to you under the Apache License, Version 2.0 (the
-       "License"); you may not use this file except in compliance
-       with the License.  You may obtain a copy of the License at
+    Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
 
-         http://www.apache.org/licenses/LICENSE-2.0
+        http://www.apache.org/licenses/LICENSE-2.0
 
-       Unless required by applicable law or agreed to in writing,
-       software distributed under the License is distributed on an
-       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-       KIND, either express or implied.  See the License for the
-       specific language governing permissions and limitations
-       under the License.
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License.
 */
 package org.apache.cordova;
 
@@ -51,7 +51,8 @@ public interface CordovaInterface {
     /**
      * Get the Android activity.
      *
-     * If a custom engine lives outside of the Activity's lifecycle the return value may be null.
+     * <p>If a custom engine lives outside of the Activity's lifecycle the return value
+     * may be null.</p>
      *
      * @return the Activity
      */
@@ -74,7 +75,7 @@ public interface CordovaInterface {
     public Object onMessage(String id, Object data);
 
     /**
-     * Returns a shared thread pool that can be used for background tasks.
+     * @return a shared thread pool that can be used for background tasks.
      */
     public ExecutorService getThreadPool();
 
@@ -89,7 +90,9 @@ public interface CordovaInterface {
     public void requestPermissions(CordovaPlugin plugin, int requestCode, String [] permissions);
 
     /**
-     * Check for a permission.  Returns true if the permission is granted, false otherwise.
+     * Check for a permission.
+     *
+     * @return true if the permission is granted, false otherwise.
      */
     public boolean hasPermission(String permission);
 
